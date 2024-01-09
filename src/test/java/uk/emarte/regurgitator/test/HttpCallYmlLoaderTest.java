5e7abdf46c2502353076a15c90470312cb0b527f
talmeym <mtalmey@gmail.com>
2024-01-09 14:18:50 +0000

    emarte uk

--- src/test/java/uk/emarte/regurgitator/test/HttpCallYmlLoaderTest.java
+++ src/test/java/uk/emarte/regurgitator/test/HttpCallYmlLoaderTest.java
@@ -2,17 +2,17 @@
  * Copyright (C) 2017 Miles Talmey.
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
-package com.emarte.regurgitator.test;
+package uk.emarte.regurgitator.test;
 
-import com.emarte.regurgitator.core.RegurgitatorException;
-import com.emarte.regurgitator.extensions.web.HttpCallYmlLoader;
+import uk.emarte.regurgitator.core.RegurgitatorException;
+import uk.emarte.regurgitator.extensions.web.HttpCallYmlLoader;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
-import static com.emarte.regurgitator.test.WebExtensionsLoaderTestExpectations.HttpCall;
-import static com.emarte.regurgitator.test.WebExtensionsLoaderTestExpectations.HttpCall_max;
+import static uk.emarte.regurgitator.core.ConfigurationFile.loadFile;
+import static uk.emarte.regurgitator.test.WebExtensionsLoaderTestExpectations.HttpCall;
+import static uk.emarte.regurgitator.test.WebExtensionsLoaderTestExpectations.HttpCall_max;
 
 public class HttpCallYmlLoaderTest extends YmlLoaderTest {
     public HttpCallYmlLoaderTest() {
